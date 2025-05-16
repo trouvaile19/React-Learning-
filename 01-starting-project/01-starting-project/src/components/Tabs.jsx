0c51a1d3ecfbfcc48df6e78019275f27508672a4
Phucpham <phamthanhqb2005@gmail.com>
2025-05-16 10:08:45 +0700

    init new project

--- 01-starting-project/01-starting-project/src/components/Tabs.jsx
+++
@@ -1,9 +0,0 @@
-export default function Tabs({children, buttons, buttonsContainer}){
-    const ButtonsContainer = buttonsContainer;
-    return(
-        <>
-            <ButtonsContainer>{buttons}</ButtonsContainer>
-            {children}
-        </>
-    );
-}  
--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,5 +1,5 @@
-export default function Log(){
+export default function Log({turns}){
     return <ol id="log">
-
+        {turns.map(turn =><li>{turn.player} <select name="" id=""></select></li>)}
     </ol>
 }
